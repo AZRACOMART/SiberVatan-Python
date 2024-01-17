28e2ebdd757c4079c042a5312ac69b841de98b0d
AZRACOMART <155827555+AZRACOMART@users.noreply.github.com>
2024-01-17 19:09:39 +0300

    Create variable-types.py

---
+++ week3/variable-types.py
@@ -0,0 +1,39 @@
+ sayi1 = 42
+ sayi2 = -17
+ print(type(sayi2))
+
+ sayi3 = 1.5
+ sayi4 = -5.5
+ print(type(sayi4))
+
+dogru_mu = True
+ yanlis_mi = False
+ print(type(yanlis_mi))
+
+ ondalik1 = 3.14
+ ondalik2 = - 0.5
+
+ sayi_float = float(sayi1)
+ print("int to float", sayi_float)
+
+ sayi_int = int(ondalik1)
+ print("float to int", sayi_int)
+
+sayi_bool = int(dogru_mu)
+print("bool to int", sayi_bool)
+
+sayi_bool = int(yanlis_mi)
+print("bool to int", sayi_bool)
+
+print(type(sayi_bool))
+
+kullanici_ad = input("Lütfen adinizi giriniz:")
+print("Hosgeldiniz", kullanici_ad)
+
+sayi5 = int(input("sayi5 giriniz:")) 
+sayi6 = int(input("sayi6 giriniz:"))
+print(sayi5 + sayi6)
+
+sayi7 = input("sayi7 gir:")
+sayi8 = input("sayi8 gir:")
+print(int(sayi7)+int(sayi8))
